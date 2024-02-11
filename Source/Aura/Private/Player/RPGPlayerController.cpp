129380f45838eb2a3f4599a4c6c1f090c19acbe8
fengqihai01 <fengqihaigg@google.com>
2024-02-12 03:17:25 +0800

    Player controller add new Actioninput

--- Source/Aura/Private/Player/RPGPlayerController.cpp
+++ Source/Aura/Private/Player/RPGPlayerController.cpp
@@ -3,3 +3,13 @@
 
 #include "Player/RPGPlayerController.h"
 
+ARPGPlayerController::ARPGPlayerController()
+{
+	bReplicates=true;
+}
+
+void ARPGPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp,Log,TEXT("111"));
+}
--- Source/Aura/Public/Player/RPGPlayerController.h
+++ Source/Aura/Public/Player/RPGPlayerController.h
@@ -6,6 +6,7 @@
 #include "GameFramework/PlayerController.h"
 #include "RPGPlayerController.generated.h"
 
+class UInputMappingContext;
 /**
  * 
  */
@@ -13,5 +14,11 @@ UCLASS()
 class AURA_API ARPGPlayerController : public APlayerController
 {
 	GENERATED_BODY()
-	
+public:
+	ARPGPlayerController();
+protected:
+	virtual  void BeginPlay() override;
+private:
+	UPROPERTY(EditAnywhere,Category="Input")
+	TObjectPtr<UInputMappingContext>  RPGInputContext;
 };
