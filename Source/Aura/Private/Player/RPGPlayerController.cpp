c39dcf9bb9fd58fa7f076251542f02d95754128f
fengqihai01 <fengqihaigg@google.com>
2024-02-12 12:52:23 +0800

    set enhanched input

--- Source/Aura/Private/Player/RPGPlayerController.cpp
+++ Source/Aura/Private/Player/RPGPlayerController.cpp
@@ -3,6 +3,7 @@
 
 #include "Player/RPGPlayerController.h"
 
+#include "EnhancedInputComponent.h"
 #include "EnhancedInputSubsystems.h"
 
 ARPGPlayerController::ARPGPlayerController()
@@ -18,10 +19,31 @@ void ARPGPlayerController::BeginPlay()
 
 	UEnhancedInputLocalPlayerSubsystem * EnhanchedSubsystem=ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
 	check(EnhanchedSubsystem);
-
+	EnhanchedSubsystem->AddMappingContext(RPGInputContext,0);
+	
 	SetShowMouseCursor(true);
 	FInputModeGameAndUI InputModeData;
 	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
 	InputModeData.SetHideCursorDuringCapture(false);
 	SetInputMode(InputModeData);
+}
+
+void ARPGPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	UEnhancedInputComponent* EnhancedInputComponent=CastChecked<UEnhancedInputComponent>(InputComponent);
+	EnhancedInputComponent->BindAction(MoveAction,ETriggerEvent::Triggered,this,&ARPGPlayerController::Move);
+}
+
+void ARPGPlayerController::Move(const FInputActionValue& InputActionValue)
+{
+	const FVector2d InputAxisVector= InputActionValue.Get<FVector2D>();
+	const FRotator ControlYawRotator=FRotator(0,GetControlRotation().Yaw,0);
+	const FVector ForwardVector=FRotationMatrix(ControlYawRotator).GetUnitAxis(EAxis::X);
+	const FVector RightVector=FRotationMatrix(ControlYawRotator).GetUnitAxis(EAxis::Y);
+	if (APawn* ControlPawn=GetPawn())
+	{
+		ControlPawn->AddMovementInput(ForwardVector,InputAxisVector.Y);
+		ControlPawn->AddMovementInput(RightVector,InputAxisVector.X);
+	}
 }
--- Source/Aura/Public/Player/RPGPlayerController.h
+++ Source/Aura/Public/Player/RPGPlayerController.h
@@ -3,10 +3,12 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "InputAction.h"
 #include "GameFramework/PlayerController.h"
 #include "RPGPlayerController.generated.h"
 
 class UInputMappingContext;
+struct FInputActionValue;
 /**
  * 
  */
@@ -18,7 +20,15 @@ public:
 	ARPGPlayerController();
 protected:
 	virtual  void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+	
 private:
 	UPROPERTY(EditAnywhere,Category="Input")
 	TObjectPtr<UInputMappingContext>  RPGInputContext;
+
+	UPROPERTY(EditAnywhere,Category="Input")
+	TObjectPtr<UInputAction> MoveAction;
+
+	UFUNCTION()
+	void Move(const FInputActionValue& InputActionValue);
 };
